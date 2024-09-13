b5c20f0dc3c50d448c63b00cc0d1b0b95fc2dfc5
Potapovych Anna <potapovych@gymvod.local>
2024-09-13 09:36:02 +0200

    neco

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 4.0f;
+            int b = 5;
+            float result = a / b;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
