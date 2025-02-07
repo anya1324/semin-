33d47d57df4e46120216070c4995783c0aed51b8
anya1324 <139865501+anya1324@users.noreply.github.com>
2025-02-07 09:28:13 +0100

    třídy

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Duel(SoundPlayer player, Enemy enemy)
+        {
+            Player player = new Player(100, 10, "ignac");
+            Enemy enemy = new Enemy(20, 2, 1);
+
+        }
+        {
+        }
+    }
+}
